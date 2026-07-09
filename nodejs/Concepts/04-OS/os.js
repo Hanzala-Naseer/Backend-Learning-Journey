78be33043f4182db98a50dfcd5eaf398ca7e4456
Hanzala Naseer <hanzalanaseer56@gmail.com>
2026-07-09 15:33:22 +0500

    practiced os module tasks

--- nodejs/Concepts/04-OS/os.js
+++ nodejs/Concepts/04-OS/os.js
@@ -1,5 +1,7 @@
 const os=require("os");
 
+const {roundToTwo}= require("./helper.js") ;
+
 let platform= os.platform();
 
 let arch=os.arch();
@@ -7,6 +9,20 @@ let arch=os.arch();
 let host=os.hostname();
 
 let user=os.userInfo();
+
+let totalMem=os.totalmem();
+totalMem=totalMem/(1024**3);
+totalMem=roundToTwo(totalMem);
+
+let freeMem=os.freemem();
+freeMem=freeMem/(1024**3);
+freeMem=roundToTwo(freeMem);
+console.log(os.cpus());
+
+let cpus= os.cpus();
+cpus=cpus.length;
+
+let uptime=os.uptime();
 
 console.log(`
 ==========================
@@ -17,5 +33,12 @@ Architecture : ${arch}
 Host Name    : ${host}
 User Name    : ${user.username}
 Home Dir     : ${user.homedir}
+Total Memory : ${totalMem} GB
+Free Memory  : ${freeMem} GB
+CPU Cores    : ${cpus}
+Uptime       : ${uptime}
+Home Dir     : ${os.homedir()}
+(os.homedir())
+
 `)
 
