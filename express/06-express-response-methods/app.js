09c0b7640252c00806f609eeddb3bdda5a18d4e1
Hanzala Naseer <hanzalanaseer56@gmail.com>
2026-07-13 11:50:42 +0500

    implement HTTP status codes with res.status()

--- express/06-express-response-methods/app.js
+++ express/06-express-response-methods/app.js
@@ -3,6 +3,8 @@ const express= require("express");
 const app= express();
 
 
+// res.send()
+
 app.get("/",(req,res)=>{
     res.send("Express Response Methods");
 })
@@ -24,14 +26,49 @@ app.get("/skills",(req,res)=>{
 })
 
 
+
+// res.json()
+
 app.get("/profile-json",(req,res)=>{
 
     const user={name:"Hanzala",email:"hanzala@gmail.com"};
-    res.send(user);
+    res.json(user);
 })
 
 
 
+// res.status()
+
+app.get("/success",(req,res)=>{
+    res.status(200).send("200 OK Projects fetched Successfully");
+})
+
+app.get("/projects",(req,res)=>{
+    res.status(201).send("201 Project created Successfully");
+})
+
+
+app.get("/bad-request",(req,res)=>{
+    res.status(400).send("400 Bad Request");
+})
+
+app.get("/unauthorized",(req,res)=>{
+    res.status(401).send("401 Unauthorized");
+})
+
+app.get("/forbidden",(req,res)=>{
+    res.status(403).send("403 Forbidden");
+})
+
+
+app.get("/not-found",(req,res)=>{
+    res.status(404).send("404 Not Found");
+})
+
+
+app.get("/server-error",(req,res)=>{
+    res.status(500).send("500 Internal Serve Error");
+})
 
 
 app.listen(3000,(req,res)=>{
